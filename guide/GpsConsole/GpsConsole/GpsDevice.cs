e1e7e120a815bb48e2fb841c41efefa0fc87a6e6
misiek <misieks@gmail.com>
2008-07-18 21:59:16 +0200

    started writing parser

---
+++ guide/GpsConsole/GpsConsole/GpsDevice.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.IO.Ports;
+
+using System.Diagnostics;
+
+namespace GpsConsole
+{
+    class GpsDevice
+    {
+        private SerialPort gpsPort = new SerialPort();
+        private System.Threading.Thread gpsListenerThread = null;
+
+        private IntPtr stateChange;
+        private IntPtr locationChange;
+
+        private NmeaParser nmea = new NmeaParser();
+
+        //public GpsDevice(IntPtr stateChange, IntPtr locationChange)
+        public GpsDevice()
+        {
+            this.stateChange = stateChange;
+            this.locationChange = locationChange;
+        }
+
+        public void Open()
+        {
+            FindPort();
+            CreateGpsListenerThread();
+        }
+
+        private void FindPort()
+        {
+            gpsPort.ReadTimeout = 300;
+            Regex rxGps = new Regex("^[$]GP");
+            for (int i = 0; i < 20; i++)
+            {
+                try
+                {
+                    gpsPort.PortName = "COM" + i;
+                    gpsPort.Open();
+                    for (int j = 0; j < 3; j++)
+                    {
+                        try
+                        {
+                            string data = gpsPort.ReadLine();
+                            if (gpsPort.IsOpen && rxGps.IsMatch(data))
+                            {
+                                Debug.WriteLine(gpsPort.PortName + ": data: " + data, this.ToString());
+                                Debug.WriteLine(gpsPort.PortName + ": connected to gps device.", this.ToString());
+                                return;
+                            }
+                        }
+                        catch (TimeoutException ex)
+                        {
+                            Debug.WriteLine(gpsPort.PortName + ": " + ex.Message, this.ToString());
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(gpsPort.PortName + ": " + ex.Message, this.ToString());
+                }
+                gpsPort.Close();
+            }
+            Debug.WriteLine("couldn't connect to Gps device! " + gpsPort.PortName, this.ToString());
+            return;
+        }
+
+        private void CreateGpsListenerThread()
+        {
+            // we only want to create the thread if we don't have one created already 
+            // and we have opened the gps device
+            if (gpsListenerThread == null && gpsPort.IsOpen)
+            {
+                // Create and start thread to listen for GPS events
+                gpsListenerThread = new System.Threading.Thread(new System.Threading.ThreadStart(Listen));
+                gpsListenerThread.Start();
+            }
+        }
+
+        private void Listen()
+        {
+            gpsPort.ReadTimeout = 50;
+            lock (this)
+            {
+                bool listening = true;
+                while (listening)
+                {
+                    string gpsMessage = "";
+                    try
+                    {
+                        gpsMessage = gpsPort.ReadLine();
+                        nmea.Parse(gpsMessage);
+                    }
+                    catch (TimeoutException ex)
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+                    
+                }
+            }
+        } 
+
+    }
+}
---
+++ guide/GpsConsole/GpsConsole/NmeaParser.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+using System.Diagnostics;
+
+namespace GpsConsole
+{
+    class NmeaParser
+    {
+        private Regex rxChecksum = new Regex("[*].+?$");
+
+        /*
+         * Message types:
+            · AAM – Waypoint Arrival Alarm,
+            · ALM – Almanac data,
+            · APA – Auto Pilot A sentence,
+            · APB – Auto Pilot B sentence,
+            · BOD – Bearing Origin to Destination,
+            · BWC – Bearing using Great Circle route,
+            · DTM – Datum being used,
+            · GGA – Fix information,
+            · GLL – Lat/Lon data,
+            · GSA – Overall Satellite data,
+            · GSV – Detailed Satellite data,
+            · MSK – Send control for a beacon receiver,
+            · MSS – Beacon receiver status information,
+            · RMA – Recommended Loran data,
+            · RMB – Recommended navigation data for gps,
+            · RMC – Recommended minimum data for gps,
+            · RTE – Route message,
+            · VTG – Vector track an Speed over the Ground,
+            · WCV – Waypoint closure velocity (Velocity Made Good),
+            · WPL – Waypoint information,
+            · XTC – Cross track error,
+            · XTE – Measured cross track error,
+            · ZTG – Zulu (UTC) time and time to go (to destination),
+            · ZDA – Date and Time.
+         * */
+        public void Parse(string gpsMessage)
+        {
+            gpsMessage = gpsMessage.Trim();
+            //gpsMessage = rxChecksum.Replace(gpsMessage, "");
+            string[] msg = gpsMessage.Split('*');
+            Console.WriteLine("gpsMessage checksum: " + msg[1]);
+            Console.WriteLine("gpsMessage: " + msg[0]);
+
+            string[] words = msg[0].Split(',');
+            switch (words[0])
+            {
+                case "$GPRMC":
+                    // RMC – Recommended minimum data for gps
+                    ParseGPRMC(words);
+                    break;
+                case "$GPGSV":
+                    // GSV – Detailed Satellite data
+                    ParseGPGSV(words);
+                    break;
+                case "$GPGSA":
+                    // GSA – Overall Satellite data
+                    ParseGPGSA(words);
+                    break;
+                default:
+                    Debug.WriteLine("unrecognized message type: " + words[0], this.ToString());
+            }
+
+        }
+
+        private void ParseGPRMC(string[] words)
+        {
+
+        }
+
+        private void ParseGPGSV(string[] words)
+        {
+
+        }
+
+        private void ParseGPGSA(string[] words)
+        {
+
+        }
+
+    }
+}
