2c7fc07ab88011e4b3e31e21a1a189eeca2366c5
misiek <misieks@gmail.com>
2008-10-11 10:49:13 +0200

    LocationData object added, show params in gui

---
+++ guide/GpsGuide/GpsGuide/Gps/LocationData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GpsGuide.Gps
+{
+    public class LocationData
+    {
+        private DateTime SatelliteTime;
+        private string LatitudeString;
+        private string LongitudeString;
+        private double Speed;
+        private double Course;
+        private string Status;
+
+        public LocationData(
+            DateTime SatelliteTime,
+            string LatitudeString,
+            string LongitudeString,
+            double Speed,
+            double Course
+        ) {
+            this.SatelliteTime      = SatelliteTime;
+            this.LatitudeString     = LatitudeString;
+            this.LongitudeString    = LongitudeString;
+            this.Speed              = Speed;
+            this.Course             = Course;
+        }
+
+        public string getLatitudeString()
+        {
+            return LatitudeString;
+        }
+
+        public string getLongitudeString()
+        {
+            return LongitudeString;
+        }
+
+        public string getSpeed()
+        {
+            return Speed.ToString();
+        }
+    }
+}
