8e4cc22cbaec683fb5499cf24d045b08d077a437
misiek <misieks@gmail.com>
2008-10-18 10:43:50 +0200

    Use Gps in TouristGuide, reference added.

--- guide/TouristGuide/TouristGuide/Form1.Designer.cs
+++ guide/TouristGuide/TouristGuide/Form1.Designer.cs
@@ -33,6 +33,10 @@ namespace TouristGuide
             this.menuItem1 = new System.Windows.Forms.MenuItem();
             this.menuItem2 = new System.Windows.Forms.MenuItem();
             this.menuItem3 = new System.Windows.Forms.MenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelPosition = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelSpeed = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // mainMenu1
@@ -53,15 +57,48 @@ namespace TouristGuide
             // 
             this.menuItem3.Text = "Start Symulator";
             // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.Text = "Position";
+            // 
+            // labelPosition
+            // 
+            this.labelPosition.Location = new System.Drawing.Point(60, 0);
+            this.labelPosition.Name = "labelPosition";
+            this.labelPosition.Size = new System.Drawing.Size(136, 20);
+            this.labelPosition.Text = "---";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.Text = "Speed";
+            // 
+            // labelSpeed
+            // 
+            this.labelSpeed.Location = new System.Drawing.Point(60, 20);
+            this.labelSpeed.Name = "labelSpeed";
+            this.labelSpeed.Size = new System.Drawing.Size(76, 20);
+            this.labelSpeed.Text = "---";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(240, 268);
+            this.Controls.Add(this.labelSpeed);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelPosition);
+            this.Controls.Add(this.label1);
             this.Menu = this.mainMenu1;
             this.Name = "Form1";
             this.Text = "TouristGuide";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
@@ -71,6 +108,10 @@ namespace TouristGuide
         private System.Windows.Forms.MenuItem menuItem1;
         private System.Windows.Forms.MenuItem menuItem2;
         private System.Windows.Forms.MenuItem menuItem3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelPosition;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelSpeed;
     }
 }
 
--- guide/TouristGuide/TouristGuide/Form1.cs
+++ guide/TouristGuide/TouristGuide/Form1.cs
@@ -5,14 +5,52 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using Gps;
 
 namespace TouristGuide
 {
     public partial class Form1 : Form
     {
+        private GpsDevice gps = new GpsDevice();
+        private EventHandler updatePosHandler;
+        private EventHandler updateSatHandler;
+
         public Form1()
         {
             InitializeComponent();
+            gps.Open();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gps.locationChanged += new GpsDevice.LocationChangedEventHandler(location);
+            //gps.satellitesChanged   += new GpsDevice.SatellitesChangedEventHandler(satellite);
+
+            updatePosHandler = new EventHandler(updateLocation);
+            //updateSatHandler = new EventHandler(updateSatellite);
+        }
+
+        private void location(GpsDevice gps)
+        {
+            Invoke(updatePosHandler);
+        }
+
+        private void satellite(GpsDevice gps)
+        {
+            Invoke(updateSatHandler);
+        }
+
+        private void updateLocation(object sender, System.EventArgs args)
+        {
+            LocationData ld = gps.getLocationData();
+            labelPosition.Text = ld.getLatitudeString() + " " + ld.getLongitudeString();
+            labelSpeed.Text = ld.getSpeed();
+        }
+
+        private void updateSatellite(object sender, System.EventArgs args)
+        {
+            //labelPosition.Text = "SATELLITE";
+        }
+
     }
 }
