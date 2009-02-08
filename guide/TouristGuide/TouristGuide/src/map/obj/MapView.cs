ae9916d8814e201246e28bc2ba7dda6d15fa90bc
misiek <misieks@gmail.com>
2009-02-08 13:57:20 +0100

    1. MapView first implemantation 2. Changed class MapPkgMapperHdd file name

--- guide/TouristGuide/TouristGuide/src/map/obj/MapView.cs
+++ guide/TouristGuide/TouristGuide/src/map/obj/MapView.cs
@@ -1,17 +1,48 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Drawing;
+using Gps;
 
 namespace TouristGuide.map.obj
 {
     public class MapView
     {
-        private int longitude;
-        private int latitude;
-        private int x;
-        private int y;
+        // gps location
+        private GpsLocation gpsLocation;
+        // relative coordinates inside map view (pixels)
+        private Point imgLocation;
+        // 0 index - center map image (longitude and latidute are inside this image)
+        // the rest as circle from top of center
+        //  8  1  2
+        //  7  0  3
+        //  6  5  4
+        private Bitmap[] images;
         // TODO: change type to more suitable
-        private object[] images;
         private object[] pois;
+
+        public MapView(GpsLocation gpsLocation, Point imgLocation, Bitmap[] images)
+        {
+            this.gpsLocation = gpsLocation;
+            this.imgLocation = imgLocation;
+            this.images = images;
+        }
+
+        public Bitmap[] getImages()
+        {
+            return this.images;
+        }
+
+        public Point getImgLocation()
+        {
+            return this.imgLocation;
+        }
+
+        public GpsLocation getGpsLocation()
+        {
+            return this.gpsLocation;
+        }
+
+
     }
 }
