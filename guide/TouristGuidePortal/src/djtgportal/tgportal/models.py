6c9c6c42bfe21f14c9c7fa7ce3288f7da97a65af
mike <misieks@gmail.com>
2009-03-28 11:47:43 +0100

    Model definition for tgportal.

--- guide/TouristGuidePortal/src/djtgportal/tgportal/models.py
+++ guide/TouristGuidePortal/src/djtgportal/tgportal/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+
+class MediaFile(models.Model):
+    """ Media file associated with POI or Detail.
+        It can be: image, mp3, avi etc.
+    """
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    file = models.FileField(upload_to='media_files')
+
+class Type(models.Model):
+    """ POI type - monument, cinema, restaurant etc. """
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+
+class Detail(models.Model):
+    """ POI detail - it can be used to give more detail information about POIs
+        (text sections, tabs etc).
+    """
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    media_files = models.ManyToManyField(MediaFile)
+    # each detail can have its own details
+    datails = models.ForeignKey('self')
+
+class POI(models.Model):
+    """ Represents Point Of Interest - geografical point on map.
+        It can be: monument, cinema, restaurant etc.
+    """
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    type = models.ForeignKey(Type)
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    media_files = models.ManyToManyField(MediaFile)
+    datails = models.ManyToManyField(Detail)
