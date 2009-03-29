60fc5f6d3ceaeba0240ee209d88440ee27232171
mike <misieks@gmail.com>
2009-03-29 14:22:36 +0200

    Django model fixed: changed relations and added MainDetail and SubDetail classes inherited from abstract Detail.

--- guide/TouristGuidePortal/src/djtgportal/tgportal/models.py
+++ guide/TouristGuidePortal/src/djtgportal/tgportal/models.py
@@ -2,36 +2,47 @@ from django.db import models
 
 
 class MediaFile(models.Model):
-    """ Media file associated with POI or Detail.
-        It can be: image, mp3, avi etc.
+    """ MediaFile class represents media associated with POI or Detail.
+        (image, mp3, avi etc)
     """
     title = models.CharField(max_length=200)
-    description = models.TextField()
+    descr = models.TextField()
     file = models.FileField(upload_to='media_files')
 
 class Type(models.Model):
-    """ POI type - monument, cinema, restaurant etc. """
-    name = models.CharField(max_length=200)
-    description = models.TextField()
-
-class Detail(models.Model):
-    """ POI detail - it can be used to give more detail information about POIs
-        (text sections, tabs etc).
+    """ Type class represents type of POI.
+        (monument, cinema, restaurant etc)
     """
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    media_files = models.ManyToManyField(MediaFile)
-    # each detail can have its own details
-    details = models.ManyToManyField('self')
+    name = models.CharField(max_length=200)
+    descr = models.TextField()
 
 class POI(models.Model):
     """ Represents Point Of Interest - geografical point on map.
-        It can be: monument, cinema, restaurant etc.
+        (monument, cinema, restaurant etc)
     """
     latitude = models.FloatField()
     longitude = models.FloatField()
     type = models.ForeignKey(Type)
     name = models.CharField(max_length=200)
-    description = models.TextField()
+    descr = models.TextField()
     media_files = models.ManyToManyField(MediaFile)
-    details = models.ManyToManyField(Detail)
+
+class Detail(models.Model):
+    """ Detail abstract class """
+    class Meta:
+        abstract = True
+    title = models.CharField(max_length=200)
+    descr = models.TextField()
+    media_files = models.ManyToManyField(MediaFile)
+
+class MainDetail(Detail):
+    """ MainDetail class represents detail of POI.
+        (text sections, tabs etc)
+    """
+    poi = models.ForeignKey(POI)
+
+class SubDetail(Detail):
+    """ SubDetail class represents detail of POI's MainDetail.
+        (sub sections, sections in tabs etc)
+    """
+    main_detail = models.ForeignKey(MainDetail)
