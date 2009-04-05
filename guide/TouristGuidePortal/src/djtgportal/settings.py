7a1a4b75672fa4ba80c3f5e8a6ee0e448b7fa9bf
mike <misieks@gmail.com>
2009-04-05 21:17:26 +0200

    1. Replaced admin/base_site.htm template for tgportal - title changed. 2. Overwritten admin/change_form.html template for tgportal's POI model edit view - added google map to allow picking coordinates. 3. Added 'help_text' to model definitions.

--- guide/TouristGuidePortal/src/djtgportal/settings.py
+++ guide/TouristGuidePortal/src/djtgportal/settings.py
@@ -21,7 +21,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # although not all choices may be available on all operating systems.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'Europe/Warsaw'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
@@ -69,6 +69,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    "/home/mike/workspace/foo/guide/TouristGuidePortal/src/djtgportal/templates"
 )
 
 INSTALLED_APPS = (
--- guide/TouristGuidePortal/src/djtgportal/tgportal/models.py
+++ guide/TouristGuidePortal/src/djtgportal/tgportal/models.py
@@ -5,9 +5,9 @@ class MediaFile(models.Model):
     """ MediaFile class represents media associated with POI or Detail.
         (image, mp3, avi etc)
     """
-    title = models.CharField(max_length=200)
-    descr = models.TextField()
-    file = models.FileField(upload_to='media_files')
+    title = models.CharField(max_length=200, help_text="Title of file.")
+    descr = models.TextField(help_text="Description of file.")
+    file = models.FileField(upload_to='media_files', help_text="Media file.")
     
     def __unicode__(self):
         return self.title
@@ -17,8 +17,8 @@ class Type(models.Model):
     """ Type class represents type of POI.
         (monument, cinema, restaurant etc)
     """
-    name = models.CharField(max_length=200)
-    descr = models.TextField()
+    name = models.CharField(max_length=200, help_text="Defines the type.")
+    descr = models.TextField(help_text="Type description.")
     
     def __unicode__(self):
         return self.name
@@ -27,12 +27,13 @@ class POI(models.Model):
     """ Represents Point Of Interest - geografical point on map.
         (monument, cinema, restaurant etc)
     """
-    latitude = models.FloatField()
-    longitude = models.FloatField()
-    type = models.ForeignKey(Type)
-    name = models.CharField(max_length=200)
-    descr = models.TextField()
-    media_files = models.ManyToManyField(MediaFile, blank=True)
+    latitude = models.FloatField(help_text="Latitude of a point.")
+    longitude = models.FloatField(help_text="Longitude of a point.")
+    type = models.ForeignKey(Type, help_text="Type of a point.")
+    name = models.CharField(max_length=200, help_text="Name of a point.")
+    descr = models.TextField(help_text="Point description.")
+    media_files = models.ManyToManyField(MediaFile, blank=True,
+                            help_text="Media files associated with a point.")
     
     def __unicode__(self):
         return self.name
@@ -41,9 +42,10 @@ class Detail(models.Model):
     """ Detail abstract class """
     class Meta:
         abstract = True
-    title = models.CharField(max_length=200)
-    descr = models.TextField()
-    media_files = models.ManyToManyField(MediaFile, blank=True)
+    title = models.CharField(max_length=200, help_text="Title of a detail.")
+    descr = models.TextField(help_text="Description of a detail.")
+    media_files = models.ManyToManyField(MediaFile, blank=True,
+                            help_text="Media files associated with a detail.")
     
     def __unicode__(self):
         return self.title
@@ -52,10 +54,11 @@ class MainDetail(Detail):
     """ MainDetail class represents detail of POI.
         (text sections, tabs etc)
     """
-    poi = models.ForeignKey(POI)
+    poi = models.ForeignKey(POI, help_text="POI characterized by a detail.")
 
 class SubDetail(Detail):
     """ SubDetail class represents detail of POI's MainDetail.
         (sub sections, sections in tabs etc)
     """
-    main_detail = models.ForeignKey(MainDetail)
+    main_detail = models.ForeignKey(MainDetail,help_text=
+                                    "MainDetail characterized by a detail.")
