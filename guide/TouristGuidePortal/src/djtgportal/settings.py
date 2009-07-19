d352f30465cbfddcc58fb16a44f45e69f48dcbbe
m <m@m-laptop.(none)>
2009-07-19 11:25:57 +0200

    1. Updated poi doc. 2. Added add/edit poi screenshot.

--- guide/TouristGuidePortal/src/djtgportal/settings.py
+++ guide/TouristGuidePortal/src/djtgportal/settings.py
@@ -35,7 +35,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = '/home/mike/workspace/djtgportal/'
+MEDIA_ROOT = '/home/m/workspace/djtgportal/'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -69,7 +69,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/mike/workspace/foo/guide/TouristGuidePortal/src/djtgportal/templates"
+    "/home/m/workspace/foo/guide/TouristGuidePortal/src/djtgportal/templates"
 )
 
 INSTALLED_APPS = (
